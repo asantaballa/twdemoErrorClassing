3d4c04b9086c381ef2ddf9aa60e30a2af2d31ae9
Alberto Santaballa <Alberto Santaballa>
2016-02-07 07:36:12 -0800

    Finish base analysis

--- qryAnalyzeLog.sql
+++ qryAnalyzeLog.sql
@@ -4,9 +4,10 @@ Go
 Set NOCOUNT On
 Go
 
-If OBJECT_ID(N'tempdb..#ErrClass', N'U') Is Not Null
-  Drop Table #ErrClass
+If OBJECT_ID(N'tempdb..#ErrClass', N'U') Is Not Null Drop Table #ErrClass
 Go
+
+-- Classify the errors --
 
 Select 
   e.ErrorLogId
@@ -14,40 +15,43 @@ Select
 , ecp.Level
 Into #ErrClass
 From ErrorLog e
-Left Outer Join ErrorMatchPatterns ecp On e.Message Like ecp.Pattern
---(
---	Select Top 1 *
---	From ErrorMatchPatterns ecp 
---	Where e.Message Like ecp.Pattern
---) ecp2 On e.Message Like ecp2.Pattern
-
+Left Outer Join ErrorMatchPatterns ecp	On e.Message Like ecp.Pattern
+										And Not Exists
+											  (	Select 1
+												From ErrorMatchPatterns ecp2
+												Where e.Message Like ecp2.Pattern
+												  And ecp2.Id < ecp.Id
+											  )
+												  
 -- Simple query --
 
---Select top 100 
---  ec.ErrorLogId
---, ecp.ID
---, ecp.Pattern
---, e.Message 
---From ErrorLog e 
---Left Outer Join #ErrClass			ec	On ec.ErrorLogId = e.ErrorLogId
---Left Outer Join ErrorMatchPatterns	ecp	On ecp.ID = ec.ErrorMatchPatternID
+Select top 100 
+  ec.ErrorLogId
+, ec.ErrorMatchPatternID
+, ecp.Pattern
+, e.Message 
+From ErrorLog e 
+Left Outer Join #ErrClass			ec	On ec.ErrorLogId = e.ErrorLogId
+Left Outer Join ErrorMatchPatterns	ecp	On ecp.ID = ec.ErrorMatchPatternID
 
 -- Sumamry Query --
 
-
 Select 
   Pattern
-, Count(*)
-, Replicate('|', Count(*) / 1)
+, ErrorMatchPatternID
+, NumMatched = Count(*)
+, Graphed = Replicate('|', Count(*) / 1)
 From
 (
 	Select 
 	  ec.ErrorLogId
-	, ecp.ID
+	, ec.ErrorMatchPatternID
 	, ecp.Pattern
 	, e.Message 
 	From ErrorLog e 
 	Left Outer Join #ErrClass			ec	On ec.ErrorLogId = e.ErrorLogId
 	Left Outer Join ErrorMatchPatterns	ecp	On ecp.ID = ec.ErrorMatchPatternID
 ) e
-Group by Pattern
+Group By 
+  Pattern
+, ErrorMatchPatternID
